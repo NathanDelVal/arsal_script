86bf7a1d45544f52dd63e8623a3c894855d5e140
NathanMad <nathan@internit.com.br>
2025-06-04 22:56:18 -0300

    more formulas 04/06

--- demo.py
+++ demo.py
@@ -58,24 +58,71 @@ try:
         last_col = planilha_alvo.used_range.last_cell.column
          
         headers = planilha_alvo.range((formulas.linha_headers, 1), (formulas.linha_headers, last_col)).value
+        
         temp_c = planilha_alvo.range((1, headers.index("Conta") + 1), (last_row, headers.index("Conta") + 1)).value
-
         temp_c = formulas.procx([c for c in temp_c[formulas.linha_headers:] if c is not None], 
-                                 lista_de_referencia.range("Q3:Q80").value, 
-                                 lista_de_referencia.range("R3:R80").value)
-
+                                 lista_de_referencia.range(f"Q{formulas.linha_headers}:Q80").value, 
+                                 lista_de_referencia.range(f"R{formulas.linha_headers}:R80").value)
         planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Cidade2"] + temp_c
         last_col += 1
 
         temp_c = planilha_alvo.range(
             (1, headers.index("Valor") + 1), (last_row, headers.index("Valor") + 1)
             ).value 
-
         temp_c = [0 if c == "N.D" else c for c in temp_c[formulas.linha_headers:]]
-
         planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Resultado independente"] + temp_c
         last_col += 1
 
+        temp_c = planilha_alvo.range((1, headers.index("Análise") + 1), (last_row, headers.index("Análise") + 1)).value
+        temp_c = formulas.procx([c for c in temp_c[formulas.linha_headers:] if c is not None], 
+                                 lista_de_referencia.range(f"T{formulas.linha_headers}:T34").value, 
+                                 lista_de_referencia.range(f"V{formulas.linha_headers}:V34").value)
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Possui Frequência na Portaria?"] + temp_c
+        last_col += 1
+        
+        temp_c = planilha_alvo.range((1, headers.index("Análise") + 1), (last_row, headers.index("Análise") + 1)).value
+        temp_c = formulas.procx([c for c in temp_c[formulas.linha_headers:] if c is not None], 
+                                 lista_de_referencia.range(f"T{formulas.linha_headers}:T34").value, 
+                                 lista_de_referencia.range(f"W{formulas.linha_headers}:W34").value)
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Possui VMP na Portaria?"] + temp_c
+        last_col += 1
+        
+        temp_c = planilha_alvo.range((1, headers.index("Análise") + 1), (last_row, headers.index("Análise") + 1)).value
+        temp_c = formulas.procx([c for c in temp_c[formulas.linha_headers:] if c is not None], 
+                                 lista_de_referencia.range(f"T{formulas.linha_headers}:T34").value, 
+                                 lista_de_referencia.range(f"U{formulas.linha_headers}:U34").value)
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Tipo Análise"] + temp_c
+        last_col += 1
+
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Responsável Análise"] 
+        last_col += 1
+        
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["ID_Conc"] 
+        last_col += 1
+        
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Justificativa  Concessionária"] 
+        last_col += 1
+        
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["ID_VI"] 
+        last_col += 1
+        
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Justificativa VI"] 
+        last_col += 1
+        
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Acatar"] 
+        last_col += 1
+        
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Etapa do Processo"] 
+        last_col += 1
+        
+        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Observações"] 
+        last_col += 1
+        
+        # temp_c = temp_c = planilha_alvo.range((1, headers.index("Formula para calculo3") + 1), (last_row, headers.index("Formula para calculo3") + 1)).value
+        # temp_c = ["Sim" if c == 1 else "Não" if c == 0 else "" for c in temp_c[formulas.linha_headers:]]
+        # planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Justificativa aceita?"] + temp_c
+        # last_col += 1        
+        
         copy_path = os.path.join(
             os.path.dirname(file_path),
             f"{os.path.splitext(file_name)[0]}_COPY_{int(datetime.datetime.timestamp(datetime.datetime.now()))}{os.path.splitext(file_name)[1]}"
