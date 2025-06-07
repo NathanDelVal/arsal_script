442088227336d509dfa5d3439f95a59aa61a870d
NathanMad <nathan@internit.com.br>
2025-06-07 12:53:16 -0300

    refactor and adjustments 06/06

--- demo.py
+++ demo.py
@@ -4,33 +4,50 @@ import xlwings as xw
 import datetime
 import tempfile
 import shutil
+import params
 import win32com.client as win32
 import formulas
+import traceback
 
 file_path = sys.argv[1]
 
-# Ensure the file exists
-if not os.path.isfile(file_path):
-    print(f"O arquivo {file_path} não existe.")
-    sys.exit(1)
+### FUNCTIONS ###
+def procx(value, lookup_array, return_array, if_not_found=None):
+    value = list(value) if not isinstance(value, list) else value
+    v = 0
+    try:
+        for v in range(len(value)):
+            index = lookup_array.index(value[v])
+            value[v] = return_array[index]
+    except Exception as e:
+        # print(f"{e} \n {lookup_array.index(value[v])}")
+        value[v] = ""
+    return value
 
-def clean_temp_folder(path):
-    for entry in os.listdir(path):
-        entry_path = os.path.join(path, entry)
-        try:
-            if os.path.isfile(entry_path) or os.path.islink(entry_path):
-                os.unlink(entry_path)
-            elif os.path.isdir(entry_path):
-                shutil.rmtree(entry_path)
-        except Exception:
-            pass  # skip files in use or protected
-
+def adjust_cols_width(sheet):
+    cols = sheet.used_range.columns
+    for i, val in enumerate(cols):
+        letter = index_to_column_letter(i + 1)
+        largest_str = [len(str(v)) for v in val.value]
+        largest_str = 14 if max(largest_str) < 14 else max(largest_str)
+        sheet[f"{letter}:{letter}"].column_width = largest_str
+    
 def index_to_column_letter(index):
     letters = ''
     while index:
         index, remainder = divmod(index - 1, 26)
         letters = chr(65 + remainder) + letters
     return letters
+#########################################################################
+planilha_alvo = "08-RST_ANL_VRF"
+lista_de_referencia = "Listas"
+planilha_de_referencia = "07-RST_ANL_JTF"
+linha_headers = 3
+##########################################################################
+# Ensure the file exists
+if not os.path.isfile(file_path):
+    print(f"O arquivo {file_path} não existe.")
+    sys.exit(1)
 
 try:
     with xw.App(visible=False) as app:
@@ -45,9 +62,9 @@ try:
         
         try:
             #variáveis de referência das tabelas
-            lista_de_referencia = dest_wb.sheets[formulas.lista_de_referencia]
-            planilha_de_referencia = dest_wb.sheets[formulas.planilha_de_referencia]
-            planilha_alvo = dest_wb.sheets[formulas.planilha_alvo]
+            lista_de_referencia = dest_wb.sheets[lista_de_referencia]
+            planilha_de_referencia = dest_wb.sheets[planilha_de_referencia]
+            planilha_alvo = dest_wb.sheets[planilha_alvo]
         except KeyError as e:
             print(f"ERRO AO ACESSAR O ARQUIVO: {e}")
             src_wb.close()
@@ -57,72 +74,103 @@ try:
         last_row = planilha_alvo.used_range.last_cell.row
         last_col = planilha_alvo.used_range.last_cell.column
          
-        headers = planilha_alvo.range((formulas.linha_headers, 1), (formulas.linha_headers, last_col)).value
+        headers = planilha_alvo.range((linha_headers, 1), (linha_headers, last_col)).value
         
-        temp_c = planilha_alvo.range((1, headers.index("Conta") + 1), (last_row, headers.index("Conta") + 1)).value
-        temp_c = formulas.procx([c for c in temp_c[formulas.linha_headers:] if c is not None], 
-                                 lista_de_referencia.range(f"Q{formulas.linha_headers}:Q80").value, 
-                                 lista_de_referencia.range(f"R{formulas.linha_headers}:R80").value)
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Cidade2"] + temp_c
-        last_col += 1
+        conta = planilha_alvo.range((linha_headers + 1, headers.index("Conta") + 1), (last_row, headers.index("Conta") + 1)).value
+        
+        cidade2 = procx([c for c in conta if c is not None], 
+                                 lista_de_referencia.range(f"Q:Q").value, 
+                                 lista_de_referencia.range(f"R:R").value)
+        
+        analise = planilha_alvo.range((linha_headers + 1, headers.index("Análise") + 1), (last_row, headers.index("Análise") + 1)).value
+        
+        parecer_analise = planilha_alvo.range((linha_headers + 1, headers.index("Parecer Análise") + 1), (last_row, headers.index("Parecer Análise") + 1)).value
+        
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Cidade2"] + cidade2
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
+        last_col = planilha_alvo.used_range.last_cell.column
 
         temp_c = planilha_alvo.range(
             (1, headers.index("Valor") + 1), (last_row, headers.index("Valor") + 1)
             ).value 
-        temp_c = [0 if c == "N.D" else c for c in temp_c[formulas.linha_headers:]]
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Resultado independente"] + temp_c
+        temp_c = [0 if c == "N.D" else c for c in temp_c[linha_headers:]]
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Resultado independente"] + temp_c
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
 
-        temp_c = planilha_alvo.range((1, headers.index("Análise") + 1), (last_row, headers.index("Análise") + 1)).value
-        temp_c = formulas.procx([c for c in temp_c[formulas.linha_headers:] if c is not None], 
-                                 lista_de_referencia.range(f"T{formulas.linha_headers}:T34").value, 
-                                 lista_de_referencia.range(f"V{formulas.linha_headers}:V34").value)
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Possui Frequência na Portaria?"] + temp_c
+        temp_c = procx([c for c in analise if c is not None], 
+                                 lista_de_referencia.range(f"T:T").value, 
+                                 lista_de_referencia.range(f"V:V").value)
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Possui Frequência na Portaria?"]  + temp_c
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        temp_c = planilha_alvo.range((1, headers.index("Análise") + 1), (last_row, headers.index("Análise") + 1)).value
-        temp_c = formulas.procx([c for c in temp_c[formulas.linha_headers:] if c is not None], 
-                                 lista_de_referencia.range(f"T{formulas.linha_headers}:T34").value, 
-                                 lista_de_referencia.range(f"W{formulas.linha_headers}:W34").value)
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Possui VMP na Portaria?"] + temp_c
+        temp_c = procx([c for c in analise if c is not None], 
+                                 lista_de_referencia.range(f"T:T").value, 
+                                 lista_de_referencia.range(f"W:W").value)
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Possui VMP na Portaria?"] + temp_c
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        temp_c = planilha_alvo.range((1, headers.index("Análise") + 1), (last_row, headers.index("Análise") + 1)).value
-        temp_c = formulas.procx([c for c in temp_c[formulas.linha_headers:] if c is not None], 
-                                 lista_de_referencia.range(f"T{formulas.linha_headers}:T34").value, 
-                                 lista_de_referencia.range(f"U{formulas.linha_headers}:U34").value)
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Tipo Análise"] + temp_c
+        temp_c = procx([c for c in analise if c is not None], 
+                                 lista_de_referencia.range(f"T:T").value, 
+                                 lista_de_referencia.range(f"U:U").value)
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Tipo Análise"] + temp_c
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
 
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Responsável Análise"] 
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Responsável Análise"] 
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["ID_Conc"] 
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["ID_Conc"] 
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Justificativa  Concessionária"] 
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Justificativa  Concessionária"] 
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["ID_VI"] 
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["ID_VI"] 
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Justificativa VI"] 
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Justificativa VI"] 
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Acatar"] 
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Acatar"] 
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Etapa do Processo"] 
+        etapa_processo = planilha_alvo.range((1, headers.index("Ponto Coleta") + 1), (last_row, headers.index("Ponto Coleta") + 1)).value
+        etapa_processo = procx([c for c in etapa_processo[linha_headers:] if c is not None], 
+                                 lista_de_referencia.range(f"Z:Z").value, 
+                                 lista_de_referencia.range(f"AH:AH").value)
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Etapa do Processo"] + etapa_processo
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Observações"] 
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Observações"] 
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
         last_col += 1
         
-        # temp_c = temp_c = planilha_alvo.range((1, headers.index("Formula para calculo3") + 1), (last_row, headers.index("Formula para calculo3") + 1)).value
-        # temp_c = ["Sim" if c == 1 else "Não" if c == 0 else "" for c in temp_c[formulas.linha_headers:]]
-        # planilha_alvo[formulas.linha_headers - 1:, last_col].options(transpose=True).value = ["Justificativa aceita?"] + temp_c
-        # last_col += 1        
+        formula1 = [f"{v1}{v2}{v3}{v4}" for v1,v2,v3,v4 in zip(cidade2,conta,analise,parecer_analise)]
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Fórmula para Cálculo"] + formula1
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
+        last_col += 1
         
+        formula2 = [f"{v1}{v2}{v3}{v4}{v5}" for v1,v2,v3,v4,v5 in zip(cidade2,conta,analise,parecer_analise, etapa_processo)] 
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Fórmula para Cálculo 2"] + formula2
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
+        last_col += 1
+        
+        planilha_alvo[linha_headers - 1:, last_col].options(transpose=True).value = ["Fórmula para Cálculo 3"] 
+        planilha_alvo[linha_headers - 1, last_col].color = (0, 0, 139)
+        last_col += 1
+        
+        adjust_cols_width(planilha_alvo)
+
         copy_path = os.path.join(
             os.path.dirname(file_path),
             f"{os.path.splitext(file_name)[0]}_COPY_{int(datetime.datetime.timestamp(datetime.datetime.now()))}{os.path.splitext(file_name)[1]}"
@@ -135,6 +183,7 @@ try:
         print("Processamento concluído com sucesso.")
 
 except Exception as e:
+    print(traceback.format_exc())
     print(f"ERRO DURANTE EXECUÇÃO: {e}")
 
 finally:
--- formulas.py
+++ formulas.py
@@ -1,85 +1,56 @@
 import xlwings as xw
 import pandas as pd
-
-### EDITEM OS VALORES AQUI ###
-planilha_alvo = "08-RST_ANL_VRF"
-lista_de_referencia = 'Listas'
-planilha_de_referencia = '07-RST_ANL_JTF'
-linha_headers = 3
-################
+import os
+import shutil
+import traceback
 
 def procx(value, lookup_array, return_array, if_not_found=None):
     value = list(value) if not isinstance(value, list) else value
-    
-    for v in range(len(value)):
-        index = lookup_array.index(value[v])
-        value[v] = return_array[index]
-        
+    v = 0
+    try:
+        for v in range(len(value)):
+            index = lookup_array.index(value[v])
+            value[v] = return_array[index]
+    except Exception as e:
+        # print(f"{e} \n {lookup_array.index(value[v])}")
+        value[v] = ""
     return value
-        
+
+def adjust_cols_width(sheet):
+    cols = sheet.used_range.columns
+    for i, val in enumerate(cols):
+        letter = index_to_column_letter(i + 1)
+        largest_str = [len(str(v)) for v in val.value]
+        largest_str = 14 if max(largest_str) < 14 else max(largest_str)
+        sheet[f"{letter}:{letter}"].column_width = largest_str
     
-# def formula1(conta):
-#     return procx(conta, lista_de_referencia.range(f"Q{linha_headers + 1}:Q80").value, lista_de_referencia.range(f"R{linha_headers + 1}:R80").value)
+def index_to_column_letter(index):
+    letters = ''
+    while index:
+        index, remainder = divmod(index - 1, 26)
+        letters = chr(65 + remainder) + letters
+    return letters
 
-# def formula2(valor):
-#     return "0" if valor == "N.D" else valor
+def find_headers_index(sht):
+    for x in range(len(sht.used_range.columns)):
+        for y in range(len(sht.used_range.columns[x].value)):
+            if sht.used_range.columns[x].value[y] is not None:
+                return y
 
-# def formulaX(analise):
-#     return procx(analise, lista_de_referencia.range(f"T{linha_headers + 1}:T{linha_headers + 1}4").value, lista_de_referencia.range(f"V{linha_headers + 1}:V{linha_headers + 1}4").value) 
+def col_letter_to_index(col_letter: str) -> int:
+    col_letter = col_letter.upper()
+    index = 0
+    for char in col_letter:
+        index = index * 26 + (ord(char) - ord('A') + 1)
+    return index
 
-# def formula4(analise):
-#     return procx(analise, lista_de_referencia.range(f"T{linha_headers + 1}:T{linha_headers + 1}4").value, lista_de_referencia.range(f"W{linha_headers + 1}:W{linha_headers + 1}4").value)
-
-def formula5(formula):
-    if formula == 2:
-        return ""
-    elif formula == 1:
-        return "Sim"
-    elif formula == 0:
-        return "Não"
-    else:
-        return ""
-
-def formula6(observacoes, formula4, freq_portaria):
-    if observacoes and formula4 == 1:
-        return "Não"
-    elif freq_portaria == "Sim":
-        return "Sim"
-    else:
-        return "Não"
-
-def formula7(iqa, freq_portaria, vmp_portaria, u4, k4, justificativa, parecer):
-    if iqa == "Não":
-        return "NA"
-    elif freq_portaria == "Sim" and vmp_portaria == "Não" and u4 == "Sim":
-        return "Conforme"
-    elif freq_portaria == "Sim" and vmp_portaria == "Sim" and u4 == "Sim" and k4 == "Conforme":
-        return "Conforme"
-    elif iqa == "Sim" and justificativa == "Sim":
-        return "Conforme"
-    elif (freq_portaria == "Sim" and vmp_portaria == "Sim" and u4 == "Sim"
-          and parecer == "Não Conforme" and (justificativa in ["", "Não"])):
-        return "Não Conforme"
-    else:
-        return "Continuar Formula"
-
-# def formula8(analise):
-#     return procx(analise, lista_de_referencia.range(f"T{linha_headers + 1}:T{linha_headers + 1}4").value, lista_de_referencia.range(f"U{linha_headers + 1}:U{linha_headers + 1}4").value)
-
-# def formula9(valor):
-#     return procx(valor, planilha_de_referencia.range(f"U5:U1008").value, planilha_de_referencia.range("V5:V1008").value)
-
-# def formula10(e4):
-#     return procx(e4, lista_de_referencia.range(f"Z{linha_headers + 1}:Z12897").value, lista_de_referencia.range(f"AH{linha_headers + 1}:AH12897").value)
-
-def formula11(cidade2, conta, analise, parecer):
-    return f"{cidade2}{conta}{analise}{parecer}"
-
-def formula12(cidade2, conta, analise, parecer, etapa):
-    return f"{cidade2}{conta}{analise}{parecer}{etapa}"
-
-def formula1X(whatever):
-    return whatever
-
-# def formula14(aa4):
-#     return procx(aa4, planilha_de_referencia.range("V5:V1561").value, planilha_de_referencia.range("W5:W1561").value, if_not_found="Sem valor")
+def clean_temp_folder(path):
+    for entry in os.listdir(path):
+        entry_path = os.path.join(path, entry)
+        try:
+            if os.path.isfile(entry_path) or os.path.islink(entry_path):
+                os.unlink(entry_path)
+            elif os.path.isdir(entry_path):
+                shutil.rmtree(entry_path)
+        except Exception:
+            pass  # skip files in use or protected
